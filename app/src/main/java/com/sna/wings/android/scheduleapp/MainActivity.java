f0da40251d6bb9c1c77780a285e1d2fc9f6fe9ce
Satoshi Mono <g36_orakul1109@yahoo.co.jp>
2021-04-18 19:07:05 +0900

    ドロワーメニューの追加。 カレンダーを表示するレイアウトを追加。

--- app/src/main/java/com/sna/wings/android/scheduleapp/MainActivity.java
+++ app/src/main/java/com/sna/wings/android/scheduleapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.sna.wings.android.scheduleapp;
 
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolBar = (Toolbar) findViewById(R.id.sToolBar);
+        setSupportActionBar(myToolBar);
+
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, myToolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 }
